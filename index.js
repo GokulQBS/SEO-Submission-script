c939b5f318ede886b60dda73e4c3d27fd56465c9
Gokul B <125027207+GokulQBS@users.noreply.github.com>
2023-03-17 18:24:29 +0530

    optimized for speed

--- index.js
+++ index.js
@@ -1,64 +1,56 @@
 function SEOSubmission(title,url,name,mail,description,category,category1,category2,category3) {
-
-let Stitle = title;
-let Surl = url;
-let Sname = name;
-let Smail = mail;
-let Sdescription = description;
-let selectvalue = category;
-let selectvalue1 = category1;
-let selectvalue2 = category2;
-let selectvalue3 = category3;
 let Rname, Rmail, Rurl, Rdescription, Rtitle = true;
 let Output = "Error in";
 let elmnt = document.querySelectorAll("select")[0];
 
-  for (var i = 0; i <= elmnt.options.length; i++) {
-    if (elmnt.options[i].innerHTML.includes(selectvalue) || elmnt.options[i].innerHTML.includes(selectvalue1) || elmnt.options[i].innerHTML.includes(selectvalue2) || elmnt.options[i].innerHTML.includes(selectvalue3) != false) {
-      elmnt.selectedIndex = i;
-      break;
-    }
-  }
   if (document.querySelectorAll("textarea").length > 0) {
-    document.querySelectorAll("textarea")[0].value = Sdescription;
+    document.querySelectorAll("textarea")[0].value = description;
   } else {
     Rdescription = false
   }
 
 
   if (document.getElementsByName("TITLE").length > 0) {
-    document.getElementsByName("TITLE")[0].value = Stitle;
+    document.getElementsByName("TITLE")[0].value = title;
   } else if (document.getElementsByName("title").length > 0) {
-    document.getElementsByName("title")[0].value = Stitle;
+    document.getElementsByName("title")[0].value = title;
   } else {
     Rtitle = false;
   }
 
   if (document.getElementsByName("URL").length > 0) {
-    document.getElementsByName("URL")[0].value = Surl;
+    document.getElementsByName("URL")[0].value = url;
   } else if (document.getElementsByName("url").length > 0) {
-    document.getElementsByName("url")[0].value = Surl;
+    document.getElementsByName("url")[0].value = url;
   } else {
     Rurl = false;
   }
 
   if (document.getElementsByName("MAIL").length > 0) {
-    document.getElementsByName("MAIL")[0].value = Smail;
+    document.getElementsByName("MAIL")[0].value = mail;
   } else if (document.getElementsByName("OWNER_EMAIL").length > 0) {
-    document.getElementsByName("OWNER_EMAIL")[0].value = Smail;
+    document.getElementsByName("OWNER_EMAIL")[0].value = mail;
   } else {
     Rmail = false;
   }
 
   if (document.getElementsByName("name").length > 0) {
-    document.getElementsByName("name")[0].value = Sname;
+    document.getElementsByName("name")[0].value = name;
   } else if (document.getElementsByName("NAME").length > 0) {
-    document.getElementsByName("NAME")[0].value = Sname;
+    document.getElementsByName("NAME")[0].value = name;
   } else if (document.getElementsByName("OWNER_NAME").length > 0) {
-    document.getElementsByName("OWNER_NAME")[0].value = Sname;
+    document.getElementsByName("OWNER_NAME")[0].value = name;
   } else {
     Rname = false;
   }
+
+  for (var i = 0; i <= elmnt.options.length; i++) {
+    if (elmnt.options[i].innerHTML.includes(category) || elmnt.options[i].innerHTML.includes(category1) || elmnt.options[i].innerHTML.includes(category2) || elmnt.options[i].innerHTML.includes(category3) != false) {
+      elmnt.selectedIndex = i;
+      break;
+    }
+  }
+
   if (Rname || Rmail || Rurl || Rdescription || Rtitle != true) {
     if (Rname == false) {
       Output = Output + ", Name";
